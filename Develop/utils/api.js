1592222d8e3c996d392058075da81b79fee55c8c
Larry_A <larryagbana@gmail.com>
2020-04-11 11:07:04 -0400

    api.js module successfully pulling data from github and reusable

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -1,5 +1,24 @@
+const axios = require('axios')
+
 const api = {
-  getUser (username) {}
+  getUser(username) {
+
+    const queryUrl = `https://api.github.com/users/${username}`
+    const config = {
+      method: 'get',
+      headers: {
+        Accept: 'application/vnd.github.v3+json'
+      }
+    }
+
+    return result = axios.get(queryUrl, config)
+    
+
+  }
+
+
 }
+
+
 
 module.exports = api
