24803c740379dd55f25d16e93a63f52edd18125b
Larry_A <larryagbana@gmail.com>
2020-04-12 01:26:32 -0400

    complete license utility

--- Develop/utils/license.js
+++ Develop/utils/license.js
@@ -1,13 +1,11 @@
-function license (type) {
-
+const license = (type) => {
 
     const links = {
-        "MIT": `https://choosealicense.com/licenses/mit/`,
-        "Academic_Free": `https://choosealicense.com/licenses/afl-3.0/`,
-        "Apache": `https://choosealicense.com/licenses/apache-2.0/`,
-        "The_Unlicense": `https://choosealicense.com/licenses/unlicense/`,
-        "MS_PL": `https://choosealicense.com/licenses/ms-pl/`,
-        "GNU_3": `https://choosealicense.com/licenses/gpl-3.0/`
+        "MIT": [`https://choosealicense.com/licenses/mit/`, `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`],
+        "Apache": [`https://choosealicense.com/licenses/apache-2.0/`, `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`],
+        "The_Unlicense": [`https://choosealicense.com/licenses/unlicense/`, `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`],
+        "Mozilla_PL_2": [`https://choosealicense.com/licenses/mpl-2.0/`, `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`],
+        "GNU_3": [`https://choosealicense.com/licenses/gpl-3.0/`, `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`]
 
     }
 
