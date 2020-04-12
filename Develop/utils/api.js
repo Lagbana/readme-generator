fa1c19502ff5400c39bbf101b135a4f64162c9d1
Larry_A <larryagbana@gmail.com>
2020-04-12 01:27:57 -0400

    error catch block implemented

--- Develop/utils/api.js
+++ Develop/utils/api.js
@@ -2,23 +2,22 @@ const axios = require('axios')
 
 const api = {
   getUser(username) {
-
-    const queryUrl = `https://api.github.com/users/${username}`
-    const config = {
-      method: 'get',
-      headers: {
-        Accept: 'application/vnd.github.v3+json'
+    try{
+      const queryUrl = `https://api.github.com/users/${username}`
+      const config = {
+        method: 'get',
+        headers: {
+          Accept: 'application/vnd.github.v3+json'
+        }
       }
+  
+      return result = axios.get(queryUrl, config)
+    } catch(err){
+      console.log(err)
     }
-
-    return result = axios.get(queryUrl, config)
     
-
   }
 
-
 }
-
-
 
 module.exports = api
